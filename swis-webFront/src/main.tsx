42b45d5c9f0d64b7b7de45c11945795892f1dc23
Omar Midani <omarmidani90@gmail.com>
2024-04-30 15:13:33 +0300

    Set up the tools

--- swis-webFront/src/main.tsx
+++ swis-webFront/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import theme from "./theme.ts";
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
