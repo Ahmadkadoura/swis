ded76250ebd9bad019af31a9d884c0e2dd3c1270
Omar Midani <omarmidani90@gmail.com>
2024-06-03 10:24:01 +0300

    Pull

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -18,33 +18,30 @@ class AuthController extends Controller
     private AuthRepository $authRepository;
     public function __construct(AuthRepository $authRepository)
     {
-        $this->AuthRepository =$authRepository;
+        $this->AuthRepository = $authRepository;
         $this->middleware(['auth:sanctum'])->only('logout');
     }
 
-    public function login(LoginRequests $request):JsonResponse
+    public function login(LoginRequests $request): JsonResponse
     {
 
-            $userData=$this->AuthRepository->login($request->validated());
+        $userData = $this->AuthRepository->login($request->validated());
 
-            return Response::Success($userData['User'],$userData['message'],$userData['code']);
-
+        return Response::Success($userData['User'], $userData['message'], $userData['code']);
     }
 
-    public function logout():JsonResponse
+    public function logout(): JsonResponse
     {
 
-            $userData=$this->AuthRepository->logout();
-            return Response::Success($userData['User'],$userData['message']);
-
+        $userData = $this->AuthRepository->logout();
+        return Response::Success($userData['User'], $userData['message']);
     }
 
-    public function register(registerRequests $request):JsonResponse
+    public function register(registerRequests $request): JsonResponse
     {
 
-            $userData=$this->AuthRepository->register($request->validated());
+        $userData = $this->AuthRepository->register($request->validated());
 
-            return Response::Success($userData['User'],$userData['message']);
-
+        return Response::Success($userData['User'], $userData['message']);
     }
 }
